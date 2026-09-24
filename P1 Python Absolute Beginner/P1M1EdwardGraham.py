acda669777c5a6f151ea5596c25c45de86bc9845
Ed Graham <edwardagraham@gmail.com>
2026-09-23 22:23:02 -0800

    updated
    doing assignments

---
+++ P1 Python Absolute Beginner/P1M1EdwardGraham.py
@@ -0,0 +1,16 @@
+# Create name check code
+
+# [ ] get input for input_test variable
+input_test = input("Enter names of people you met in the last 24 hours: ")
+
+# [ ] print "True" message if "John" is in the input or False message if not
+print("john" in input_test.lower())
+
+# [ ] print True message if your name is in the input or False if not
+print("edward" in input_test.lower())
+
+# [ ] Challenge: Check if another person's name is in the input - print message
+print("mary" in input_test.lower())
+
+# [ ] Challenge: Check if a fourth person's name is in the input - print message
+print("alice" in input_test.lower())
